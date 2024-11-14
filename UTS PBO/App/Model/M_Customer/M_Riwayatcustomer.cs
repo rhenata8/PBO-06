18831366f3a00bddc370d74992de6721a650484a
aalfinabila <aalfinabila11@gmail.com>
2024-11-14 12:23:39 +0700

    Update M_Riwayatcustomer.cs

--- UTS PBO/App/Model/M_Customer/M_Riwayatcustomer.cs
+++ UTS PBO/App/Model/M_Customer/M_Riwayatcustomer.cs
@@ -8,5 +8,17 @@ namespace UTS_PBO.App.Model.M_Customer
 {
     internal class M_Riwayatcustomer
     {
+          [Key]
+          public int id { get; set; }
+          [Required]
+          public string username { get; set; }
+          [Required]
+          public string makanan { get; set; }
+          [Required]
+          public string minuman { get; set; }
+          [Required]
+          public int nomer_meja { get; set; }
+          [Required]
+          public int harga { get; set; }
     }
 }

f44c99bf703d3a9faaa778c8ee230701058cfb2e
rhenata8 <145882610+rhenata8@users.noreply.github.com>
2024-11-14 12:17:14 +0700

    M_DataMeja.cs

--- UTS PBO/App/Model/M_Admin/M_DataMeja.cs
+++ UTS PBO/App/Model/M_Admin/M_DataMeja.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace UTS_PBO.App.Model.M_Admin
 {
     internal class M_DataMeja
     {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public int Nomor_meja { get; set; }
+        [Required]
+        public int Kapasitas { get; set; }
+        [Required]
+        public int Lantai { get; set; }
+        [Required]
+        public Enum Status { get; set; }
+
+
     }
 }

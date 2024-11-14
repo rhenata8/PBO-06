e377ae1469d97da04fd542fa1a6b904c98e95145
aalfinabila <aalfinabila11@gmail.com>
2024-11-14 12:22:50 +0700

    Update M_Registcustomer.cs

--- UTS PBO/App/Model/M_Customer/M_Registcustomer.cs
+++ UTS PBO/App/Model/M_Customer/M_Registcustomer.cs
@@ -8,5 +8,15 @@ namespace UTS_PBO.App.Model.M_Customer
 {
     internal class M_Registcustomer
     {
+         [Key]
+         public int id { get; set; }
+         [Required]
+         public string username { get; set; }
+         [Required]
+         public string password { get; set; }
+         [Required]
+         public string email { get; set; }
+         [Required]
+         public int nomer_hp { get; set; }
     }
 }

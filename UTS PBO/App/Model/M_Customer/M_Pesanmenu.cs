84e1ede2ea3397537d50dec272291aea80c61b04
aalfinabila <aalfinabila11@gmail.com>
2024-11-14 12:21:43 +0700

    Update M_Pesanmenu.cs

--- UTS PBO/App/Model/M_Customer/M_Pesanmenu.cs
+++ UTS PBO/App/Model/M_Customer/M_Pesanmenu.cs
@@ -8,5 +8,15 @@ namespace UTS_PBO.App.Model.M_Customer
 {
     internal class M_Pesanmenu
     {
+         [Key]
+         public int id { get; set; }
+         [Required]
+         public string makanan { get; set; }
+         [Required]
+         public string minuman { get; set; }
+         [Required]
+         public int harga { get; set; }
+         [Required]
+         public DateTime tanggal { get; set; }
     }
 }

d290816fdb616af13c62ac62e00a84796a245540
aalfinabila <aalfinabila11@gmail.com>
2024-11-15 17:25:46 +0700

    Update M_pesananAdmin.cs

--- UTS PBO/App/Model/M_Admin/M_pesananAdmin.cs
+++ UTS PBO/App/Model/M_Admin/M_pesananAdmin.cs
@@ -8,17 +8,19 @@ namespace UTS_PBO.App.Model.M_Admin
 {
     internal class M_pesananAdmin
     {
-          [Key]
-          public int id { get; set; }
-          [Required]
-          public string nama { get; set; }
-          [Required]
-          public string makanan { get; set; }
-          [Required]
-          public string minuman { get; set; }
-          [Required]
-          public int nomer_meja { get; set; }
-          [Required]
-          public int harga { get; set; }
+         [Key]
+         public int id { get; set; }
+         [Required]
+         public string nama { get; set; }
+         [Required]
+         public string makanan { get; set; }
+         [Required]
+         public string minuman { get; set; }
+         [Required]
+         public int nomer_meja { get; set; }
+         [Required]
+         public int harga { get; set; }
+         [Required]
+         public Enum status { get; set; }
     }
 }

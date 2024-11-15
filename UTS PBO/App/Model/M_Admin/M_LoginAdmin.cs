cec2d044cc60e49985cc54e2ccc7fb49576c6322
venyraa <167503345+venyraa@users.noreply.github.com>
2024-11-15 17:19:18 +0700

    Update M_LoginAdmin.cs

--- UTS PBO/App/Model/M_Admin/M_LoginAdmin.cs
+++ UTS PBO/App/Model/M_Admin/M_LoginAdmin.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace UTS_PBO.App.Model.M_Admin
 {
     internal class M_LoginAdmin
     {
+        [Key]
+        public string username { get; set; }
+        [Required]
+        public string password { get; set; }
+        
     }
 }

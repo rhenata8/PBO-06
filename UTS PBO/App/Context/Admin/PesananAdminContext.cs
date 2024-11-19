9ac136bb6ade66a2a6cc30586a9d14fa4771d3a1
aalfinabila <aalfinabila11@gmail.com>
2024-11-19 09:14:22 +0700

    Update PesananAdminContext.cs

--- UTS PBO/App/Context/Admin/PesananAdminContext.cs
+++ UTS PBO/App/Context/Admin/PesananAdminContext.cs
@@ -8,5 +8,56 @@ namespace UTS_PBO.App.Context.Admin
 {
     internal class PesananAdminContext
     {
+         private NpgsqlConnection conn = new NpgsqlConnection();
+
+ public DataTable GetAllReservations()
+ {
+     NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM reservations", conn.GetConnection());
+     NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(command);
+     DataTable table = new DataTable();
+     adapter.Fill(table);
+     return table;
+ }
+
+ public bool MakeReservation(int nomer, string nama, string makanan, string minuman, int harga, DateTime tanggal, Enum status)
+ {
+     NpgsqlCommand command = new NpgsqlCommand();
+     string query = "INSERT INTO reservations (nomer_meja, nama, makanan, minuman, harga, tanggal, status) " +
+                    "VALUES (@nomer_meja, @nama, @makanan, @minuman, @harga, @tanggal, 'Active')";
+     command.CommandText = query;
+     command.Connection = conn.GetConnection();
+
+     command.Parameters.AddWithValue("@nomer_meja", nomer);
+     command.Parameters.AddWithValue("@nama", nama);
+     command.Parameters.AddWithValue("@makanan", makanan);
+     command.Parameters.AddWithValue("@minuman", minuman);
+     command.Parameters.AddWithValue("@harga", harga);
+     command.Parameters.AddWithValue("@tanggal", tanggal);
+     command.Parameters.AddWithValue("@status", status);
+
+     conn.OpenConnection();
+     bool isInserted = command.ExecuteNonQuery() == 1;
+     conn.CloseConnection();
+     return isInserted;
+ }
+
+ public void CancelExpiredReservations()
+ {
+     string query = "UPDATE reservations SET status = 'Canceled' " +
+                    "WHERE date_out < @currentDateTime AND status = 'Active'";
+     NpgsqlCommand command = new NpgsqlCommand(query, conn.GetConnection());
+     command.Parameters.AddWithValue("@currentDateTime", DateTime.Now);
+
+     conn.OpenConnection();
+     int canceledCount = command.ExecuteNonQuery();
+     conn.CloseConnection();
+
+     Console.WriteLine($"{canceledCount} expired reservations have been canceled.");
+ }
+
+ public void CheckReservationsOnAdminLogin()
+ {
+     CancelExpiredReservations();
+ }
     }
 }

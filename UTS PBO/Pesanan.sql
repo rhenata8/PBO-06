e658cc0ccf619db8669302f7628628c94a94c667
venyraa <167503345+venyraa@users.noreply.github.com>
2024-11-19 17:19:23 +0700

    Pesanan.sql

--- UTS PBO/Pesanan.sql
+++ UTS PBO/Pesanan.sql
@@ -1,13 +1,13 @@
 create type reservation_status as enum ('Active','Canceled')
 
 CREATE TABLE reservations (
-    id SERIAL PRIMARY KEY,                      -- ID unik untuk setiap reservasi
-    nomor_meja INT NOT NULL,                    -- Nomor meja yang dipesan
-    tanggal TIMESTAMP NOT NULL,                 -- Tanggal dan waktu reservasi
-    nama_customer VARCHAR(100) NOT NULL,        -- Nama pelanggan
-    makanan VARCHAR(100) NOT NULL,              -- Nama makanan yang dipesan
-    minuman VARCHAR(100) NOT NULL,              -- Nama minuman yang dipesan
-    harga NUMERIC(10, 2) NOT NULL,              -- Harga total (dengan desimal)
-    status reservation_status DEFAULT 'Active'  -- Status reservasi, default 'Active'
+    id SERIAL PRIMARY KEY,                      
+    nomor_meja INT NOT NULL,                    
+    tanggal TIMESTAMP NOT NULL,                
+    nama_customer VARCHAR(100) NOT NULL,        
+    makanan VARCHAR(100) NOT NULL,             
+    minuman VARCHAR(100) NOT NULL,              
+    harga NUMERIC(10, 2) NOT NULL,              
+    status reservation_status DEFAULT 'Active'  
 );
 select * from reservations

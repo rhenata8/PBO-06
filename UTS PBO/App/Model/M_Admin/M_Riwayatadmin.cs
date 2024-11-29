5874c5df16c1e73cf46eddb7be3f25bcf9756ab2
venyraa <167503345+venyraa@users.noreply.github.com>
2024-11-29 12:03:00 +0700

    M_Riwayatadmin.cs

--- UTS PBO/App/Model/M_Admin/M_Riwayatadmin.cs
+++ UTS PBO/App/Model/M_Admin/M_Riwayatadmin.cs
@@ -1,26 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UTS_PBO.App.Model.M_Admin
 {
-    internal class M_Riwayatadmin
+    internal class PesananAdmin
     {
-          [Key]
-          public int id { get; set; }
-          [Required]
-          public string nama { get; set; }
-          [Required]
-          public string makanan { get; set; }
-          [Required]
-          public string minuman { get; set; }
-          [Required]
-          public int nomer_meja { get; set; }
-          [Required]
-          public int harga { get; set; }
-          [Required]
-          public DateTime tanggal { get; set; } 
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public int nomor_meja { get; set; }
+        [Required]
+        public DateTime tanggal { get; set; }
+        [Required]
+        public string nama_customer { get; set; }
+        [Required]
+        public string makanan { get; set; }
+        [Required]
+        public string minuman { get; set; }
+        public int harga { get; set; }
+        public string status  { get; set; }
     }
 }
